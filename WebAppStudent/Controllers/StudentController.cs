13559f3d092f109cc2cf4833c5ee4608052f2a91
stLordMord <stupidlord999@gmail.com>
2020-02-16 03:49:34 +0300

    Добавьте файлы проекта.

---
+++ WebAppStudent/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace WebAppStudent.Controllers
+{
+    public class StudentController : Controller
+    {
+        public readonly StudentService _studentService;
+
+        public StudentController()
+        {
+            _studentService = new StudentService();
+        }
+
+        // GET: Student
+        public async Task<ActionResult> Index()
+        {
+            var students = await _studentService.GetStudents();
+
+            return View(students);
+        }
+
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+
+        public ActionResult EditStudent()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteStudent()
+        {
+            return RedirectToAction("index"); // возвращает на другой метод
+        }
+    }
+}
